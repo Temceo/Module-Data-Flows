0f947c2efade5e6f0a97070d6825a243fd98c254
Martin Mwaka <martinmwaka@hotmail.com>
2026-07-27 21:55:03 +0100

    complete task for exercise-2

--- Sprint-1/destructuring/exercise-2/exercise.js
+++ Sprint-1/destructuring/exercise-2/exercise.js
@@ -70,3 +70,36 @@ let hogwarts = [
     occupation: "Teacher",
   },
 ];
+
+// line under headings
+const logLine = () => {
+  console.log("=".repeat(20));
+};
+
+const findGryffindorResidents = (residents) => {
+  console.log("GRYFFINDOR RESIDENTS");
+  logLine();
+  return residents
+    .reduce((acc, resident) => {
+      const { firstName, lastName, house } = resident;
+      if (house === "Gryffindor") acc.push(`${firstName} ${lastName}`);
+      return acc;
+    }, [])
+    .join("\n");
+};
+
+console.log(findGryffindorResidents(hogwarts));
+
+const findTeachersWithPets = (residents) => {
+  console.log("TEACHERS WITH PETS");
+  logLine();
+  return residents
+    .reduce((acc, resident) => {
+      const { firstName, lastName, pet, occupation } = resident;
+      if (pet && occupation === "Teacher") acc.push(`${firstName} ${lastName}`);
+      return acc;
+    }, [])
+    .join("\n");
+};
+
+console.log(findTeachersWithPets(hogwarts));
